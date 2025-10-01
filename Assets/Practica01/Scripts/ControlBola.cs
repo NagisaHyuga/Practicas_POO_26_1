a07c78cac752359f3606379ce388a2208806bcf0
Montalvo Polet <esmeralda2406004@gmail.com>
2025-10-01 10:38:22 -0600

    Se agregan pinos animados
    Se agregaron pinos animados, movimiento para apuntar con la bola y una cámara dinámica.

--- Assets/Practica01/Scripts/ControlBola.cs
+++ Assets/Practica01/Scripts/ControlBola.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ControlBola : MonoBehaviour
 {
-    
+    public Transform CamaraPrincipal;
+
     public Rigidbody rb;
+
+    //Variables para apuntar
+    public float velocidadDeApuntado = 5f;
+    public float limiteIzquierdo = -2f;
+    public float limiteDerecho = 2f;
+
+
+
     public float fuerzaDeLanzamiento = 1000f;
 
+    private bool haSidoLanzada = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +28,44 @@ public class ControlBola : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-       if(Input.GetKeyDown(KeyCode.Space))
+    {      //Expresión: mientras que haSidoLanzada sea falso puedes disparar.
+        if (haSidoLanzada==false)
         {
-            rb.AddForce(Vector3.forward*fuerzaDeLanzamiento);  
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Lanzar();
+            }
         }
-
     }
-}
+
+    void Apuntar()
+    {
+        //1. Leer un input horizontal de tipo Axis te permite registrar
+        // entradas con las teclas A y D, la flecha izquiera y flecha dertecha
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        //2. Mover la bola hacia los lados
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
+
+        //3. Delimitar el movimiento de la bola
+        Vector3 posicionActual = transform.position; //Me permite saber cual es la posición actual en la escena.
+
+        posicionActual.x =Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+
+        transform.position = posicionActual;
+    }
+
+    void Lanzar()
+    {
+        haSidoLanzada = true;
+        rb.AddForce(Vector3.forward* fuerzaDeLanzamiento);
+
+        if(CamaraPrincipal !=null)
+        {
+            CamaraPrincipal.SetParent(transform);
+        }
+    }
+    
+
+} // Bienvenido a la entrada al infierno
