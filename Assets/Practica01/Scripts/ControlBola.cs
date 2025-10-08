b63d425eaa26c395ab20df022fc90c55f6cdc682
Montalvo Polet <esmeralda2406004@gmail.com>
2025-10-08 10:47:27 -0600

    Añadimos códigos
    Agregamos códigos

--- Assets/Practica01/Scripts/ControlBola.cs
+++ Assets/Practica01/Scripts/ControlBola.cs
@@ -5,8 +5,6 @@ using UnityEngine.Rendering;
 
 public class ControlBola : MonoBehaviour
 {
-    public Transform CamaraPrincipal;
-
     public Rigidbody rb;
 
     //Variables para apuntar
@@ -20,52 +18,75 @@ public class ControlBola : MonoBehaviour
 
     private bool haSidoLanzada = false;
 
+    //TODO: Referencia a la cámara y score
+    public CameraFollow cameraFollow;
+    public ScoreManager scoreManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PISTA: Obtener el componente Rigidbody de esta bola
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
-    {      //Expresión: mientras que haSidoLanzada sea falso puedes disparar.
-        if (haSidoLanzada==false)
+    {
+        //Expresion:mientras que haSidoLanzada sea falso puedes disparar.
+        if (haSidoLanzada == false)
         {
             Apuntar();
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Lanzar();
+                LanzarBola();
             }
         }
     }
 
     void Apuntar()
     {
-        //1. Leer un input horizontal de tipo Axis te permite registrar
-        // entradas con las teclas A y D, la flecha izquiera y flecha dertecha
+        //1.leer un input horizontal de tipo Axis te permite registrar
+        // entradas co las teclas A y D
+        //PISTA: Obtener el movimiento horizontal(-1 a 1)
         float inputHorizontal = Input.GetAxis("Horizontal");
 
-        //2. Mover la bola hacia los lados
+        //2 mOVER HACIA LOS LADOS LA BOLA
         transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
 
-        //3. Delimitar el movimiento de la bola
-        Vector3 posicionActual = transform.position; //Me permite saber cual es la posición actual en la escena.
-
-        posicionActual.x =Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+        //3 delimitar movimiento de la boola transform.position permite ver la posicion actual de la bola en la escena
+        Vector3 posicionActual = transform.position;
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
 
         transform.position = posicionActual;
     }
 
-    void Lanzar()
+    void LanzarBola()
     {
         haSidoLanzada = true;
-        rb.AddForce(Vector3.forward* fuerzaDeLanzamiento);
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+        //Comprobar si el rigidbody existe antes de usarlo
+        //Esta comrpobacion evita errores NullReferenceException
+        //Si olvidamos arrastrar el rigidbody o el componente fue eliminado
+        //el sistema no deberia intentar usarlo
 
-        if(CamaraPrincipal !=null)
+        //PISTA: Iniciar seguimiento de la camara
+        if (cameraFollow != null) cameraFollow.IniciarSeguimiento();
+      }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //si colisiona con un pino
+        if (collision.gameObject.CompareTag("Pin"))
         {
-            CamaraPrincipal.SetParent(transform);
+            //detener el seguimiento de camara
+            if (cameraFollow != null) cameraFollow.DetenerSeguimiento();
+                //calcular puntaje tras un pequenio retraso
+            if (scoreManager != null) Invoke("CalcularPuntaje", 0f);
         }
     }
-    
-
-} // Bienvenido a la entrada al infierno
+    void CalcularPuntaje()
+    {
+        //Llamar al ScoreManager para actulizar puntos
+       scoreManager.CalcularPuntaje();
+    }
+} //Bienvenido a la entrada al infierno
